7e49ed8fe22f5b0ef8aafe31d325a44aa0983c8a
luigimiraglia <96987926+luigimiraglia@users.noreply.github.com>
2025-07-21 19:01:00 +0200

    fixed deployment bug

--- app/[lezione]/page.tsx
+++ app/[lezione]/page.tsx
@@ -1,9 +1,7 @@
-interface Props {
-  params: {
-    lezione: string;
-  };
+interface RouteParams {
+  lezione: string;
 }
 
-export default function LezionePage({ params }: Props) {
+export default function LezionePage({ params }: { params: RouteParams }) {
   return <h1>Lezione: {params.lezione}</h1>;
 }
